c7ea3567e21e9af06816759cc1b8ba97a03d0eb1
Dickson Souza <Dickson.Souza@rhimagnesita.com>
2021-07-11 16:31:29 -0300

    Added code from lesson about filtering

--- RestrictionOperators/Program.cs
+++ RestrictionOperators/Program.cs
@@ -8,12 +8,14 @@ namespace RestrictionOperators
 {
   class Program
   {
+    private const string SEPARATOR = "===============================================================";
     static void Main(string[] args)
     {
 
-      // LINQ query structure
       // This sample uses where to find all elements of an array less than 5. 
       // It demonstrates the components of a query, including a where clause that filters for small numbers.
+      printHeaders("LINQ query structure");
+
       List<int> numbers = new List<int>(){5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
 
       var lowNums = from num in numbers
@@ -25,6 +27,22 @@ namespace RestrictionOperators
       {
         Console.WriteLine(x);
       }
+
+      // This sample uses where to find all products that are out of stock. Its where clause examines a property of the items in the input sequence.
+      printHeaders("Filter elements on a property");
+      List<Product> products = Restrictions.GetProductList();
+
+      var soldOutProducts = from prod in products
+                            where prod.UnitsInStock == 0
+                            select prod;
+
+      Console.WriteLine("Sold out products:");
+      foreach (var product in soldOutProducts)
+      {
+          Console.WriteLine($"{product.ProductName} is sold out!");
+      }
+    }
+  
     private static void printHeaders(string text)
     {
       Console.WriteLine(SEPARATOR);
